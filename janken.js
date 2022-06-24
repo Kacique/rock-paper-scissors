231cbc0582f08b9e6a21dc5b3074173b5a462c45
William Brand <wbrand90@gmail.com>
2022-06-23 17:10:42 -0700

    Completed game functionality

--- janken.js
+++ janken.js
@@ -6,6 +6,15 @@ let computerSelection;
 
 let result;
 
+const btn = document.querySelectorAll("button");
+
+btn.forEach((button) => {
+  button.addEventListener("click", () => {
+    playerSelection = button.id;
+    game();
+  });
+});
+
 function computerPlay() {
   let compChoice = Math.floor(Math.random() * 3);
 
@@ -22,18 +31,13 @@ function computerPlay() {
     default:
       break;
   }
-
-  //console.log("Computer choice: " + compChoice);
 }
 
-function letsPlayAGame(playerSelection, computerSelection) {
-  playerSelection = prompt("Choose your weapon!: ");
-  computerSelection = computerPlay();
-
-  let choice = playerSelection.toUpperCase();
+function letsPlayAGame(playerChoice, computerChoice) {
+  let choice = playerChoice;
 
   if (choice == "ROCK") {
-    switch (computerSelection) {
+    switch (computerChoice) {
       case "ROCK":
         return (result = "It's a tie");
         break;
@@ -49,7 +53,7 @@ function letsPlayAGame(playerSelection, computerSelection) {
         break;
     }
   } else if (choice == "PAPER") {
-    switch (computerSelection) {
+    switch (computerChoice) {
       case "ROCK":
         result = "You win! Paper beats Rock!";
         return (playerScore += 1);
@@ -65,7 +69,7 @@ function letsPlayAGame(playerSelection, computerSelection) {
         break;
     }
   } else if (choice == "SCISSORS") {
-    switch (computerSelection) {
+    switch (computerChoice) {
       case "ROCK":
         result = "You lose! Rock beats Scissors!";
         return (computerScore += 1);
@@ -86,12 +90,30 @@ function letsPlayAGame(playerSelection, computerSelection) {
 }
 
 function game() {
-  for (let i = 0; i < 5; i++) {
-    letsPlayAGame(playerSelection, computerSelection);
-    console.log(result);
+  letsPlayAGame(playerSelection, computerPlay());
+  console.log(result);
+  console.log(`Current score is You:${playerScore} Computer:${computerScore}`);
+  if (playerScore == 5 || computerScore == 5) {
+    console.log("Game Over");
+    playerScore = 0;
+    computerScore = 0;
   }
-
-  return `The final score is You:${playerScore} Computer:${computerScore}`;
 }
 
-console.log(game());
+/**
+ * In our UI, the player should be able to play the game by clicking on buttons rather than typing their answer in a prompt.
+For now, remove the logic that plays exactly five rounds.
+Create three buttons, one for each selection. 
+
+Add an event listener to the buttons that call your playRound 
+function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+
+Add a div for displaying results and change all of your console.logs into DOM methods.
+
+Display the running score, and announce a winner of the game once one player reaches 5 points.
+
+You will likely have to refactor (rework/rewrite) your original code to make it work for this. 
+That’s OK! Reworking old code is an important part of a programmer’s life.
+
+Once you’re all done with your UI and made sure everything’s satisfactory, commit your changes to the rps-ui branch.
+ */
